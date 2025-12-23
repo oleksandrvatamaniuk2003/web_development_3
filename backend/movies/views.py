eea7f876fed3db6f7241c3073cdf7f5d4f0dc48c
Oleksandr Vatamaniuk <oleksandr_vatamaniuk@knu.ua>
2025-12-23 16:17:34 +0200

    backend

--- backend/movies/views.py
+++ backend/movies/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Movie
+from .serializers import MovieSerializer
 
-# Create your views here.
+
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['genre', 'director']
+    search_fields = ['title']
